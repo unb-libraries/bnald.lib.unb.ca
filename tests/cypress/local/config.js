4cfefce0d824193b2e139c610c1f546a0371c718
Patrick Schilf <pschilf@unb.ca>
2022-08-31 17:00:08 -0300

    Remove cypress config, rename spec folder

--- tests/cypress/local/config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  e2e: {
-    reporter: "cypress/reporter/JsonDBReporter.js",
-    specPattern: 'cypress/e2e/**/*.cy.js',
-    baseUrl: "http://bnald.lib.unb.ca"
-  }
-})
