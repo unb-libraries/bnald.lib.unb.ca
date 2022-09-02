1a294b14fbfbf3c4d3e76899067a618f30ff154c
Patrick Schilf <pschilf@unb.ca>
2022-09-02 15:11:32 -0300

    BNALD-64 Replace placeholder spec with real test case

--- tests/cypress/default/e2e/bnald.cy.js
+++ tests/cypress/default/e2e/bnald.cy.js
@@ -1,9 +1,16 @@
 describe("British North American Legislation Database (BNALD)", () => {
-  context("Front page", () => {
-    it('Should not be found', () => {
-      cy.visit("/", {failOnStatusCode: false})
-      cy.get('h2.page-header')
-        .contains('Page not found')
+  context("Front page search form", () => {
+    it('Should forward to legislation search page', () => {
+      cy.intercept('/legislation/search*', request => {
+        expect(request.query.query).to.equal('moose preservation')
+        request.destroy()
+      }).as('searchRequest')
+
+      cy.visit("/")
+      cy.get('form').within(() => {
+        cy.get('input[type="text"]').type('moose preservation')
+      }).submit()
+      cy.wait('@searchRequest')
     })
   })}
 )
