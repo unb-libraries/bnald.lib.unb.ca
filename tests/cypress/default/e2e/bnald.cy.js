4ea4fc601893d7f537f57fc86f4e6297cc129980
Patrick Schilf <pschilf@unb.ca>
2022-09-07 10:37:54 -0300

    BNALD-64 Remove form submt interception

--- tests/cypress/default/e2e/bnald.cy.js
+++ tests/cypress/default/e2e/bnald.cy.js
@@ -1,10 +1,7 @@
 describe("British North American Legislation Database (BNALD)", () => {
   context("Front page search form", () => {
     it('Should forward to legislation search page', () => {
-      cy.intercept('/legislation/search*', request => {
-        expect(request.query.query).to.equal('moose preservation')
-        request.destroy()
-      }).as('searchRequest')
+      cy.intercept('/legislation/search?query=moose+preservation').as('searchRequest')
 
       cy.visit("/")
       cy.get('form').within(() => {
