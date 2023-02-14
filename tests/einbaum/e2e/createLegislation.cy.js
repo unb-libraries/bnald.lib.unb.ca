0b113bd0abd100b91563cd21ee0750f14963621f
Patrick Schilf <pschilf@unb.ca>
2023-02-14 11:58:36 -0400

    Refactor "create legislation" spec

--- tests/einbaum/e2e/createLegislation.cy.js
+++ tests/einbaum/e2e/createLegislation.cy.js
@@ -1,36 +1,43 @@
+import users from '../fixtures/users.json'
+
+const editor = users.find(user => user.roles.includes('bnald_editor'))
+const formPath = '/legislation/add'
+
 describe('Creating a "Piece of Legislation"', () => {
-  context('as "BNALD editor"', () => {
-    before(() => {
-      cy.loginAs('bnald_editor')
-      cy.visit('/legislation/add')
+  context('Form access', () => {
+    users.forEach(user => {
+      const granted = user.name === editor.name
+      specify(`${user.name}: ${granted ? 'granted' : 'denied'}`, () => {
+        cy.loginAs(user.name)
+        cy.request({url: formPath, failOnStatusCode: false})
+          .its('status')
+          .should('match', granted ? /20\d/ : /40\d/)
+      });
     })
-    
+  })
+
+  context('Form submission', () => {
+    before(() => {
+      cy.loginAs(editor.name)
+      cy.visit(formPath)
+    })
+
     it('should result in a new "Legislation" entity', () => {
-      cy.get('[data-test="origin"]')
-        .enter("Acts of the General Assembly of His Majesty's Province of New Brunswick")
-      cy.get('[data-test="title"]')
-        .enter('An Act to incorporate the Richibucto Boom Company. Passed 17th June 1867.')
-      cy.get('[data-test="chapter"]')
-        .enter('30 Victoria - Chapter 85')
-      cy.get('[data-test="year"]')
-        .enter(1867)
-      cy.get('[data-test="article_count"]')
-        .enter(18)
-      cy.get('[data-test="province"]')
-        .enter('New Brunswick')
-      cy.get('[data-test="summary"]')
-        .enter('This Act incorporates the Richibucto Boom Company, their powers, and the responsibilities surrounding their equipment.')
-      cy.get('[data-test="full_text"]')
-        .enter("Whereas the erection of a Boom or Booms at or near the bridge over the Richibucto River near Anthony Cail's, in the County of Kent, will be a great benefit...")
-      cy.get('[data-test="pdf_original"]')
-        .enter('original.pdf')
-      cy.get('[data-test="pdf_transcribed"]')
-        .enter('transcribed.pdf')
-      cy.get('[data-test="jurisdictional_relevance"]')
-        .enter('Local')
-      cy.get('[data-test="concepts"]')
-        .enter('Natural Resources')
-      
+      Object.entries({
+        origin: "Acts of the General Assembly of His Majesty's Province of New Brunswick",
+        title: 'An Act to incorporate the Richibucto Boom Company. Passed 17th June 1867.',
+        chapter: '30 Victoria - Chapter 85',
+        year: 1867,
+        article_count: 18,
+        province: 'New Brunswick',
+        summary: 'This Act incorporates the Richibucto Boom Company, their powers, and the responsibilities surrounding their equipment.',
+        full_text: "Whereas the erection of a Boom or Booms at or near the bridge over the Richibucto River near Anthony Cail's, in the County of Kent, will be a great benefit...",
+        pdf_original: 'original.pdf',
+        pdf_transcribed: 'transcribed.pdf',
+        jurisdictional_relevance: 'Local',
+        concepts: 'Natural Resources',
+      }).forEach(([field, value]) => cy.get(`[data-test="${field}"`).enter(value))
+
       cy.get('[data-test="submit"]')
         .click()
 
@@ -38,23 +45,6 @@ describe('Creating a "Piece of Legislation"', () => {
         .should('match', /legislation\/act-incorporate-richibucto-boom-company-passed-17th-june-1867(-\d+)?/)
       cy.get('[data-test*="status"]')
         .contains('Created the An Act to incorporate the Richibucto Boom Company. Passed 17th June 1867. Legislation.')
-    })
-  })
-
-  context('as another logged-in user', () => {
-    it('should result in an "Access denied" page', () => {
-      cy.loginAs('user')
-      cy.visit('/legislation/add', {failOnStatusCode: false})
-      cy.get('[data-test="page-title"]')
-        .contains('Access denied')
-    })
-  })
-
-  context('as an anonymous user', () => {
-    it('should result in an "Access denied" page', () => {
-      cy.visit('/legislation/add', {failOnStatusCode: false})
-      cy.get('[data-test="page-title"]')
-        .contains('Access denied')
     })
   })
 })
