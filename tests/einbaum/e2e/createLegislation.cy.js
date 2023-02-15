f666d8137cb153590baffec8d311f2a77869eb88
Patrick Schilf <pschilf@unb.ca>
2023-02-15 17:08:31 -0400

    Remove form access context from "create legislation" spec

--- tests/einbaum/e2e/createLegislation.cy.js
+++ tests/einbaum/e2e/createLegislation.cy.js
@@ -22,83 +22,55 @@ const editor = users.find(user => user.roles.includes('bnald_editor'))
 const formPath = '/legislation/add'
 
 describe('Creating a "Piece of Legislation"', () => {
-  context('Form access', () => {
-    users.forEach(user => {
-      const granted = user.name === editor.name
-      specify(`${user.name} should ${!granted ? 'not ' : ''}see form link`, () => {
-        cy.loginAs(user.name)
-        cy.visit('/legislation/recently-added')
-        if (granted) {
-          cy.get('[data-test="local-action-entity-legislation-add-form"] a')
-            .its('0.href')
-            .should('match', RegExp(formPath))
-        }
-        else {
-          cy.get('[data-test="local-action-entity-legislation-add-form"]')
-            .should('not.exist')
-        }
-      });
+  let realPath = ''
 
-      specify(`${user.name} should be ${granted ? 'granted' : 'denied'} form access`, () => {
-        cy.loginAs(user.name)
-        cy.request({url: formPath, failOnStatusCode: false})
-          .its('status')
-          .should('match', granted ? /20\d/ : /40\d/)
-      })
-    })
+  before(() => {
+    cy.loginAs(editor.name)
+    cy.visit(formPath)
   })
 
-  context.only('Form submission', () => {
-    let realPath = ''
-
-    before(() => {
-      cy.loginAs(editor.name)
-      cy.visit(formPath)
+  specify(`is created via form submission`, () => {
+    Object.entries({
+      origin,
+      title,
+      chapter,
+      year,
+      article_count,
+      province,
+      summary,
+      full_text,
+      pdf_original,
+      pdf_transcribed,
+      jurisdictional_relevance,
+      concepts,
+    }).forEach(([field, value]) => {
+      cy.get(`[data-test="${field}"`)
+        .enter(value)
     })
 
-    it('should result in a new "Legislation" entity', () => {
-      Object.entries({
-        origin,
-        title,
-        chapter,
-        year,
-        article_count,
-        province,
-        summary,
-        full_text,
-        pdf_original,
-        pdf_transcribed,
-        jurisdictional_relevance,
-        concepts,
-      }).forEach(([field, value]) => {
-        cy.get(`[data-test="${field}"`)
-          .enter(value)
-      })
+    cy.get('[data-test="submit"]')
+      .click()
 
-      cy.get('[data-test="submit"]')
-        .click()
+    cy.location('pathname').then(pathname => realPath = pathname)
+      .should('contain', path)
+    cy.get('[data-test*="status"]')
+      .contains(`Created the ${title} Legislation`)
+  })
 
-      cy.location('pathname').then(pathname => realPath = pathname)
-        .should('contain', path)
-      cy.get('[data-test*="status"]')
-        .contains(`Created the ${title} Legislation`)
-    })
+  specify('tops list of most recently added', () => {
+    cy.visit('/legislation/recently-added')
+    cy.get('[data-test="view-item-0-view_legislation"]')
+      .should('contain', title)
+  });
 
-    it('should appear as the most recently added item', () => {
-      cy.visit('/legislation/recently-added')
-      cy.get('[data-test="view-item-0-view_legislation"]')
-        .should('contain', title)
-    });
+  specify(`shows up in search results`, () => {
+    cy.visit('/legislation/search')
+    cy.get('[data-test="form-element-query"]')
+      .type(query)
+    cy.get('[data-test="submit"]')
+      .click()
 
-    it(`should show up in search results for "${query}"`, () => {
-      cy.visit('/legislation/search')
-      cy.get('[data-test="form-element-query"]')
-        .type(query)
-      cy.get('[data-test="submit"]')
-        .click()
-
-      cy.get('[data-test*="view-item"]')
-        .find(`[href="${realPath}"]`)
-    })
+    cy.get('[data-test*="view-item"]')
+      .find(`[href="${realPath}"]`)
   })
 })
