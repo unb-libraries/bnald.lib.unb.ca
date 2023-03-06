59bdd4241a2a11aae2d511b2028259f588a61032
Patrick Schilf <pschilf@unb.ca>
2023-03-06 13:55:26 -0400

    Update front page search form test

--- tests/einbaum/e2e/search.cy.js
+++ tests/einbaum/e2e/search.cy.js
@@ -1,12 +1,16 @@
+import { winterRoadsLegislation } from '../fixtures/legislations.json'
+
 describe('Search', () => {
+  const { query }  = winterRoadsLegislation
+
   context("Front page search form", () => {
     it('Should forward to legislation search page', () => {
       cy.visit("/")
       cy.get('form').within(() => {
-        cy.get('input[type="text"]').type('moose preservation')
+        cy.get('input[type="text"]').type(query)
       }).submit()
       cy.url()
-        .should('match', /\/legislation\/search\?query=moose\+preservation/)
+        .should('match', new RegExp(`\/legislation\/search\\?query=${query.replace(" ", "\\+")}`))
     })
   })
 })
