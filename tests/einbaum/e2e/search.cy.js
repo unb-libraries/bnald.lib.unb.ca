2315851d564204410a0784da745f8bec7e4b65a2
Patrick Schilf <pschilf@unb.ca>
2023-03-10 16:03:04 -0400

    Update front search spec

--- tests/einbaum/e2e/search.cy.js
+++ tests/einbaum/e2e/search.cy.js
@@ -6,9 +6,12 @@ describe('Search', () => {
   context("Front page search form", () => {
     it('Should forward to legislation search page', () => {
       cy.visit("/")
-      cy.get('form').within(() => {
-        cy.get('input[type="text"]').type(query)
-      }).submit()
+
+      cy.get('input[type="text"]')
+        .type(query)
+      cy.get('[data-test="submit"]')
+        .click()
+
       cy.url()
         .should('match', new RegExp(`\/legislation\/search\\?query=${query.replace(" ", "\\+")}`))
     })
