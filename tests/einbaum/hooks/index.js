94d588b64fb9df9a917734ed8a65426e51a808a8
Patrick Schilf <pschilf@unb.ca>
2023-03-13 15:17:18 -0300

    Provide dockworker path in "beforeAll" hook

--- tests/einbaum/hooks/index.js
+++ tests/einbaum/hooks/index.js
@@ -2,8 +2,8 @@ module.exports = {
   dockworkerMigrate: {
     type: 'before',
     fn: () => {
-      cy.exec('dockworker migrate-rollback legislations', {log: false})
-      cy.exec('dockworker migrate-import --tags=e2e', {log: false})
+      cy.exec('vendor/bin/dockworker migrate-rollback legislations', {log: false})
+      cy.exec('vendor/bin/dockworker migrate-import --tags=e2e', {log: false})
     }
   },
 }
