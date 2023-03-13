fc283cf494c28b24eb83d8edc4871229b05d690d
Patrick Schilf <pschilf@unb.ca>
2023-03-13 16:02:57 -0300

    Remove einbaum tests

--- tests/einbaum/hooks/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  dockworkerMigrate: {
-    type: 'before',
-    fn: () => {
-      cy.exec('vendor/bin/dockworker migrate-rollback legislations', {log: false})
-      cy.exec('vendor/bin/dockworker migrate-import --tags=e2e', {log: false})
-    }
-  },
-}
