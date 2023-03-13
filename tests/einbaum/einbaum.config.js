f86ac30680ae33f3baaf4d21f2209527ad8ba057
Patrick Schilf <pschilf@unb.ca>
2023-03-13 14:05:29 -0300

    Update einbaum baseUrl

--- tests/einbaum/einbaum.config.js
+++ tests/einbaum/einbaum.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@unb-libraries/einbaum')
 module.exports = defineConfig({
   cypress: {
     e2e: {
-      baseUrl: 'http://local-bnald.lib.unb.ca:3099',
+      baseUrl: 'http://localhost:3099',
     },
   },
   plugins: {
